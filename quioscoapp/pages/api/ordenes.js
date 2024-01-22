68efa714fc0035e5af66e442961dbae321f3ab30
Lucia de Oliveira <lucia.deoliveira@orageloops.com>
2024-01-22 09:58:36 -0300

    Finished tenth project

--- quioscoapp/pages/api/ordenes.js
+++ quioscoapp/pages/api/ordenes.js
@@ -1,18 +1,29 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-export default async function handler(req, res){
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
 
-    const prisma = new PrismaClient()
+  if (req.method === "GET") {
+    // Obetener Ordenes
+    const ordenes = await prisma.orden.findMany({
+      where: {
+        estado: false,
+      },
+    });
+    res.status(200).json(ordenes);
+  }
 
-    if(req.method === 'POST'){
-        const orden = await prisma.orden.create({
-            data: {
-                nombre: req.body.nombre,
-                total: req.body.total,
-                pedido: req.body.pedido,
-                fecha: req.body.fecha,
-            },
-        });
-        res.json(orden)
-    }
+  // Crear Ordenes
+
+  if (req.method === "POST") {
+    const orden = await prisma.orden.create({
+      data: {
+        nombre: req.body.nombre,
+        total: req.body.total,
+        pedido: req.body.pedido,
+        fecha: req.body.fecha,
+      },
+    });
+    res.status(200).json({ orden });
+  }
 }
